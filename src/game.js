90f26eab0f7c744b8f6ed54346702f566cfcc5fb
Alex McHardy <alexmchardy@gmail.com>
2021-04-28 09:50:56 -0600

    Remove game dependency on Cells class

--- src/game.js
+++ src/game.js
@@ -10,8 +10,8 @@ function getPoint(coord) {
 
 function getNeighborCoords(x, y, w, h) {
 	const coords = [];
-	for (let i = Math.max(0, x - 1); i <= Math.min(w, x + 1); i++) {
-		for (let j = Math.max(0, y - 1); j <= Math.min(h, y + 1); j++) {
+	for (let i = Math.max(0, x - 1); i <= x + 1; i++) {
+		for (let j = Math.max(0, y - 1); j <= y + 1; j++) {
 			if (i !== x || j !== y) {
 				coords.push(getCoord(i, j));
 			}
@@ -20,11 +20,11 @@ function getNeighborCoords(x, y, w, h) {
 	return coords;
 }
 
-function run(cells) {
+function run(live) {
 	const tally = {};
-	cells.live.forEach(([x, y]) => {
+	live.forEach(([x, y]) => {
 		const cellCoord = getCoord(x, y);
-		const neighborCoords = getNeighborCoords(x, y, cells.width, cells.height);
+		const neighborCoords = getNeighborCoords(x, y);
 
 		// Increase each neighboring cell's neighbor count by 1
 		neighborCoords.forEach((coord) => {
@@ -52,13 +52,7 @@ function run(cells) {
 		[]
 	);
 
-	const nextGenCells = new Cells({
-		height: cells.height,
-		width: cells.width,
-		living: nextGen,
-	});
-
-	return nextGenCells;
+	return nextGen;
 }
 
 export { run };
--- src/game.test.js
+++ src/game.test.js
@@ -1,37 +1,31 @@
 import { run } from './game';
-import Cells from './cells';
 import * as stillLifes from './fixtures/still-lifes';
 import * as oscillators from './fixtures/oscillators';
 import * as spaceships from './fixtures/spaceships';
 
 const ordinal = ['1st', '2nd', '3rd', '4th', '5th'];
 function testGenerations(h, w, gens) {
-	let currentGen = new Cells({
-		height: h,
-		width: w,
-		living: gens.shift(),
-	});
+	let currentGen = gens.shift();
 	gens.forEach((gen, i) => {
 		test(`${ordinal[i + 1]} generation`, () => {
 			currentGen = run(currentGen);
-			expect(currentGen.live.sort()).toEqual(gen.sort());
+			expect(currentGen.sort()).toEqual(gen.sort());
 		});
 	});
 }
 
 describe('Game', () => {
-	describe('runner', () => {
+	describe('run', () => {
 		test('handles dead culture', () => {
-			const cells = new Cells();
-			const nextGen = run(cells);
-			expect(nextGen.live).toEqual([]);
+			const live = [];
+			const nextGen = run(live);
+			expect(nextGen).toEqual([]);
 		});
 
 		test('handles single live cell', () => {
-			const cells = new Cells();
-			cells.live = [[0, 2]];
-			const nextGen = run(cells);
-			expect(nextGen.live).toEqual([]);
+			const live = [[0, 2]];
+			const nextGen = run(live);
+			expect(nextGen).toEqual([]);
 		});
 
 		describe('handles still lifes', () => {
